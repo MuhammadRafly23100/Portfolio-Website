df2ccfa472ead30e6993ba98f094a613b98ce088
MuhammadRafly23100 <mraflyyydwiii@gmail.com>
2025-07-10 22:59:46 +0700

    Final attempt to fix FormSubmit form

--- src/App.jsx
+++ src/App.jsx
@@ -91,25 +91,27 @@ function App() {
         <h1 className="text-4xl mb-2 font-bold text-center" data-aos="fade-up" data-aos-duration="1500" data-aos-once="true">Kontak</h1>
         <p className="text-base/loose text-center mb-10 opacity-50" data-aos="fade-up" data-aos-duration="1500" data-aos-delay="300" data-aos-once="true">Mari terhubung dengan saya</p>
         <form action="https://formsubmit.co/dc5bdead92d5c0e05aa0929641" method="POST" className="bg-zinc-800 p-10 sm:w-fit w-full mx-auto rounded-md" autoComplete="off" data-aos="fade-up" data-aos-duration="1500" data-aos-delay="500" data-aos-once="true">
-         <input type="hidden" name="_to" value="mraflyyydwiii@gmail.com" />
-          <div className="flex flex-col gap-6">
-            <div className="flex flex-col gap-2">
-              <label className="font-semibold">Nama Lengkap</label>
-              <input type="text" name="nama" placeholder="Masukkan Nama..." className="border border-zinc-500 p-2 rounded-md" required></input>
-            </div>
-            <div className="flex flex-col gap-2">
-              <label className="font-semibold">Email</label>
-              <input type="email" name="email" placeholder="Masukkan Email..." className="border border-zinc-500 p-2 rounded-md" required></input>
-            </div>
-            <div className="flex flex-col gap-2">
-              <label htmlFor="pesan" className="font-semibold">Pesan</label>
-              <textarea name="pesan" id="pesan" cols="45" rows="7" placeholder="Pesan..." className="border border-zinc-500 p-2 rounded-md" required></textarea>
-            </div>
-            <div className="text-center">
-              <button type="submit" className="bg-violet-700 p-3 rounded-lg w-full cursor-pointer border border-zinc-600 hover:bg-violet-600">Kirim Pesan</button>
-            </div>
-          </div>
-        </form>
+    {/* Input tersembunyi untuk tujuan email */}
+    <input type="hidden" name="_to" value="mraflyyydwiii@gmail.com" />
+
+    <div className="flex flex-col gap-6">
+        <div className="flex flex-col gap-2">
+            <label className="font-semibold">Nama Lengkap</label>
+            <input type="text" name="nama" placeholder="Masukkan Nama..." className="border border-zinc-500 p-2 rounded-md" required></input>
+        </div>
+        <div className="flex flex-col gap-2">
+            <label className="font-semibold">Email</label>
+            <input type="email" name="email" placeholder="Masukkan Email..." className="border border-zinc-500 p-2 rounded-md" required></input>
+        </div>
+        <div className="flex flex-col gap-2">
+            <label htmlFor="pesan" className="font-semibold">Pesan</label>
+            <textarea name="pesan" id="pesan" cols="45" rows="7" placeholder="Pesan..." className="border border-zinc-500 p-2 rounded-md" required></textarea>
+        </div>
+        <div className="text-center">
+            <button type="submit" className="bg-violet-700 p-3 rounded-lg w-full cursor-pointer border border-zinc-600 hover:bg-violet-600">Kirim Pesan</button>
+        </div>
+    </div>
+</form>
       </div>
       {/* Kontak */}
     </>
